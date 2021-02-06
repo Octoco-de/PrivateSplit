d62c68eede1a21f9aaaeaf55ea8b288f8ea6fb5a
gonxas <development@gonxas.com>
2021-02-06 20:06:58 -0300

    Customized envirorment configuration

---
+++ app/src/Screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, Animated, Settings, DeviceEventEmitter, FlatList, View } from 'react-native'
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ backgroundColor: 'red', width: '100%', height: '100%' }} />
+        )
+    }
+}
+
+export default (HomeScreen)
